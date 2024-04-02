45e621af0c3b797b6c34fba8c7b4f8f601d7f581
JiJiCodes <69917943+jijicodes@users.noreply.github.com>
2024-04-02 13:40:17 -0500

    feat: adding footer to the pages (#4)
    * feat: add footer
    
    * updating code review- directory name change, updating the mui to the latest version, and removed some unnecesary code

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,13 @@
 'use client'
 import type { FC, PropsWithChildren } from 'react'
-import { useTheme, Stack, Button, Typography, Avatar, AppBar, Toolbar } from '@mui/material'
+import { useTheme, Stack, Button, Typography, Avatar, AppBar, Toolbar, IconButton } from '@mui/material'
 import { AccountMenu } from './AccountMenu'
 import { usePathname, useRouter } from 'next/navigation'
 import { useModal } from './provider'
 import { LeaveWebsiteModal } from './modals/LeaveWebsiteModal'
 import { KadoModal } from './modals/KadoModal'
+import { Language, X } from '@mui/icons-material'
+import { Discord } from './icons/Discord'
 
 export const Layout: FC<PropsWithChildren> = ({ children }) => {
   const theme = useTheme()
@@ -53,11 +55,19 @@ export const Layout: FC<PropsWithChildren> = ({ children }) => {
       <Stack component="main" direction="column" minHeight='100vh' p={2}>
         {children}
       </Stack>
-      <AppBar component="footer" position="fixed" sx={{ position: 'relative', bottom: 0 }}>
-        <Toolbar>
-          <Stack direction="row">
-
-          </Stack>
+      <AppBar component='footer' position='fixed' sx={{ position: 'relative', bottom: 0 }}>
+        <Toolbar sx={{ flex: 1, direction: 'row', justifyContent: 'center' }}>
+            <Stack direction='row' spacing={4} justifyContent='center' width='100%'>
+              <IconButton color='primary' aria-label='website' onClick={() => window.open('https://ash-dao.net/', '_blank')}>
+                <Language />
+              </IconButton>
+              <IconButton color='primary' aria-label='X Icon' onClick={() => window.open('https://twitter.com/_ASH_DAO', '_blank')}>
+                <X />
+              </IconButton>
+              <IconButton aria-label='Discord Icon' onClick={() => window.open('https://discord.com/invite/PEJzbV25mX', '_blank')}>
+                <Discord />
+              </IconButton>
+            </Stack>
         </Toolbar>
       </AppBar>
     </>
