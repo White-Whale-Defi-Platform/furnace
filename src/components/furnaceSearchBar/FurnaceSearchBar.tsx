5d9e267331e2bf3e1527539499e374752d613c4a
jijicodes <sjieun11@gmail.com>
2024-04-03 16:57:06 -0500

    feat: cleaned some code

--- src/components/furnaceSearchBar/FurnaceSearchBar.tsx
+++ src/components/furnaceSearchBar/FurnaceSearchBar.tsx
@@ -1,49 +1,84 @@
 import React, { type FC, useState } from 'react'
 import { assets } from 'chain-registry'
-import { Avatar, Button, Box, InputAdornment, Stack, Dialog, TextField, Typography, DialogContent, Chip } from '@mui/material'
+import {
+  Avatar,
+  Button,
+  Box,
+  InputAdornment,
+  Stack,
+  Dialog,
+  TextField,
+  Typography,
+  DialogContent,
+  Chip
+} from '@mui/material'
 import { Search } from '@mui/icons-material'
 
 // This is dummy data to display the assets on the search select drop down
 const getAssetInfo = {
-  migaloo: [{
-    from: assets.find(({ chain_name }) => chain_name === 'migaloo')?.assets.find(({ symbol }) => symbol === 'WHALE'),
-    to: assets.find(({ chain_name }) => chain_name === 'migaloo')?.assets.find(({ symbol }) => symbol === 'ASH')
-  },
-  {
-    from: assets.find(({ chain_name }) => chain_name === 'migaloo')?.assets.find(({ symbol }) => symbol === 'GUPPY'),
-    // I used the FABLE as a filler since there is no GASH in the chain registry
-    to: assets.find(({ chain_name }) => chain_name === 'migaloo')?.assets.find(({ symbol }) => symbol === 'FABLE')
-  }],
-  chihuahua: [{
-    from: assets.find(({ chain_name }) => chain_name === 'chihuahua')?.assets.find(({ symbol }) => symbol === 'HUAHUA'),
-    to: assets.find(({ chain_name }) => chain_name === 'chihuahua')?.assets.find(({ symbol }) => symbol === 'PUPPY')
-  }]
+  migaloo: [
+    {
+      from: assets
+        .find(({ chain_name: chainName }) => chainName === 'migaloo')
+        ?.assets.find(({ symbol }) => symbol === 'WHALE'),
+      to: assets
+        .find(({ chain_name: chainName }) => chainName === 'migaloo')
+        ?.assets.find(({ symbol }) => symbol === 'ASH')
+    },
+    {
+      from: assets
+        .find(({ chain_name: chainName }) => chainName === 'migaloo')
+        ?.assets.find(({ symbol }) => symbol === 'GUPPY'),
+      // I used the FABLE as a filler since there is no GASH in the chain registry
+      to: assets
+        .find(({ chain_name: chainName }) => chainName === 'migaloo')
+        ?.assets.find(({ symbol }) => symbol === 'FABLE')
+    }
+  ],
+  chihuahua: [
+    {
+      from: assets
+        .find(({ chain_name: chainName }) => chainName === 'chihuahua')
+        ?.assets.find(({ symbol }) => symbol === 'HUAHUA'),
+      to: assets
+        .find(({ chain_name: chainName }) => chainName === 'chihuahua')
+        ?.assets.find(({ symbol }) => symbol === 'PUPPY')
+    }
+  ]
 }
 
 const FurnaceSearchBar: FC = () => {
   // format the getAssetInfo into array then flatten every asset info into one arry
-  const assetOptions = Object.entries(getAssetInfo).flatMap(([chain, chainInfo]) => chainInfo.map((asset) => ({ ...asset, chain })))
+  const assetOptions = Object.entries(getAssetInfo).flatMap(
+    ([chain, chainInfo]) => chainInfo.map((asset) => ({ ...asset, chain }))
+  )
   const [open, setOpen] = useState(false)
   const [searchString, setSearchString] = useState('')
 
   // if searchString is set then do the filtering otherwise just use all assets
-  const filteredAssetOptions = (searchString.length > 0)
-    ? assetOptions.filter(({ from, to, chain }) => {
-      return (
-        // check if search string matches with from asset symbol
-        Boolean(from?.symbol.toLowerCase().includes(searchString)) ||
-        // check if search string matches with to asset symbol
-        Boolean(to?.symbol.toLowerCase().includes(searchString)) ||
-        // check if search string matches with chain name
-            chain.toLowerCase().includes(searchString)
-      )
-    })
-    : assetOptions
+  const filteredAssetOptions =
+    searchString.length > 0
+      ? assetOptions.filter(({ from, to, chain }) => {
+        return (
+          // check if search string matches with from asset symbol
+          Boolean(from?.symbol.toLowerCase().includes(searchString)) ||
+          // check if search string matches with to asset symbol
+          Boolean(to?.symbol.toLowerCase().includes(searchString)) ||
+          // check if search string matches with chain name
+          chain.toLowerCase().includes(searchString)
+        )
+      })
+      : assetOptions
   return (
     <Stack sx={{ width: '40%' }}>
       <Button
-      focusRipple
-      sx={{ border: 'unset', background: '#18181b', color: 'white', '&:hover': { bgcolor: 'black' } }}
+        focusRipple
+        sx={{
+          border: 'unset',
+          background: '#18181b',
+          color: 'white',
+          '&:hover': { bgcolor: 'black' }
+        }}
         component="label"
         role={undefined}
         variant="outlined"
@@ -52,42 +87,74 @@ const FurnaceSearchBar: FC = () => {
       >
         Search Assets
       </Button>
-       <Dialog
-        fullWidth
-        open={open}
-        onClose={() => setOpen(false)} >
-            <DialogContent>
-                <Stack gap={2}>
-                <TextField focused fullWidth id="outlined-basic" label="Search for Assets" variant="outlined"
-                    InputProps={{
-                      endAdornment: (
-                        <InputAdornment position="start">
-                           <Search />
-                         </InputAdornment>
-                      )
-                    }}
-                    onChange={(e) => setSearchString(e.target.value.toLowerCase())}
-                />
-                {filteredAssetOptions.length > 0
-                  ? filteredAssetOptions.map((option) => (
-                    <Button href={`/${option.chain}/${(option.from?.symbol)?.toLowerCase()}`} key={`${option.from?.symbol}${option.to?.symbol}`} sx={{ justifyContent: 'space-between' }}>
-                      <Stack direction="row"><Box display={'flex'}key={`${option.from?.symbol}${option.to?.symbol}`}>
-      <Stack direction="row">
-        <Avatar alt="token img" sx={{ width: 24, height: 24, mr: 1 }} src={option.from?.logo_URIs?.png ?? option.from?.logo_URIs?.svg}/>
+      <Dialog fullWidth open={open} onClose={() => setOpen(false)}>
+        <DialogContent>
+          <Stack gap={2}>
+            <TextField
+              focused
+              fullWidth
+              id="outlined-basic"
+              label="Search for Assets"
+              variant="outlined"
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="start">
+                    <Search />
+                  </InputAdornment>
+                )
+              }}
+              onChange={(e) => setSearchString(e.target.value.toLowerCase())}
+            />
+            {filteredAssetOptions.length > 0
+              ? filteredAssetOptions.map(({ chain, from, to }) => {
+                const fromSymbol = from?.symbol ?? ''
+                const toSymbol = to?.symbol ?? ''
+                const fromLogo = from?.logo_URIs?.png ?? from?.logo_URIs?.svg
+                const toLogo = to?.logo_URIs?.png ?? to?.logo_URIs?.svg
 
-      <Avatar alt="token img" sx={{ width: 24, height: 24, mr: 0.5, left: '-10px', display: 'absolute' }} src={option.to?.logo_URIs?.png ?? option.to?.logo_URIs?.svg}/>
-      </Stack>
-      <Typography color='white'>{`${option.from?.symbol} / ${option.to?.symbol}`}</Typography>
-      </Box></Stack> <Chip variant="outlined" size="small" label={option.chain}/></Button>))
-                  : 'No Furnace Found'}
+                return (
+                  <Button
+                    href={`/${chain}/${fromSymbol.toLowerCase()}`}
+                    key={`${fromSymbol}${toSymbol}`}
+                    sx={{ justifyContent: 'space-between' }}
+                  >
+                    <Stack direction="row">
+                      <Box
+                        display={'flex'}
+                        key={`${fromSymbol}${toSymbol}`}
+                      >
+                        <Stack direction="row">
+                          <Avatar
+                            alt="token img"
+                            sx={{ width: 24, height: 24, mr: 1 }}
+                            src={fromLogo} />
 
-                 <Stack>
-                 </Stack>
-                </Stack>
-            </DialogContent>
+                          <Avatar
+                            alt="token img"
+                            sx={{
+                              width: 24,
+                              height: 24,
+                              mr: 0.5,
+                              left: '-10px',
+                              display: 'absolute'
+                            }}
+                            src={toLogo} />
+                        </Stack>
+                        <Typography color="white">{`${fromSymbol} / ${toSymbol}`}</Typography>
+                      </Box>
+                    </Stack>
+                    <Chip
+                      variant="outlined"
+                      size="small"
+                      label={chain} />
+                  </Button>
+                )
+              })
+              : 'No Furnace Found'}
+          </Stack>
+        </DialogContent>
       </Dialog>
     </Stack>
-
   )
 }
 
