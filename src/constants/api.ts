c3864c924e80a4f2a073f855b6b6f4d2bc89862b
jijicodes <sjieun11@gmail.com>
2024-04-03 16:33:15 -0500

    feat: removed repeated code

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -3,13 +3,6 @@ export const FETCH_INTERVAL = 6_000
 export const ENDPOINTS = {
   migaloo: {
     rpc: ['https://migaloo-rpc.polkachu.com'],
-    rest: ['https://migaloo-api.polkachu.com'],
-    furnaces: [
-      // burn whale to ash
-      'migaloo1erul6xyq0gk6ws98ncj7lnq9l4jn4gnnu9we73gdz78yyl2lr7qqrvcgup',
-
-      // burn guppy to gash
-      'migaloo1r9x8fz4alekzr78k42rpmr9unpa7egsldpqeynmwl2nfvzexue9sn8l5rg'
-    ]
+    rest: ['https://migaloo-api.polkachu.com']
   }
 }
