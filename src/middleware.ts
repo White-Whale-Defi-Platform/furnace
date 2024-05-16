b2a2be637db72a3d107e020d78937ab128063f18
jijicodes <sjieun11@gmail.com>
2024-05-15 21:28:31 -0500

    feat: added middleware.ts

---
+++ src/middleware.ts
@@ -0,0 +1,8 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { ENDPOINTS } from './constants'
+
+export function middleware (request: NextRequest): NextResponse | undefined {
+  const pathSections = request.nextUrl.pathname.split('/')
+  if (pathSections.length === 3 && !(pathSections[1] in ENDPOINTS)) { return NextResponse.redirect(new URL('/', request.url)) }
+}
