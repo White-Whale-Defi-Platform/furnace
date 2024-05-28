7f28ff91db3b3152b94df72405865ebee6a885ea
jijicodes <sjieun11@gmail.com>
2024-05-28 11:42:01 -0500

    chore: removed unused file

--- src/components/provider/Provider.tsx
+++ src/components/provider/Provider.tsx
@@ -7,35 +7,41 @@ import { ThemeProvider } from '@mui/material'
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 import { osmosis, chihuahua } from 'graz/chains'
 import { GrazProvider, WalletType } from 'graz'
-import { SnackbarProvider, ModalProvider, AppProvider, UserProvider } from '@/components'
+import { SnackbarProvider, ModalProvider, AppProvider } from '@/components'
 
 const queryClient = new QueryClient()
 
 export const Provider: FC<PropsWithChildren> = ({ children }) => {
-  const chihuahua_chain = { ...chihuahua, rpc: 'https://chihuahua-rpc.polkachu.com', rest: 'https://rest.cosmos.directory/chihuahua' }
-  const osmosis_chain = { ...osmosis, rpc: 'https://osmosis-rpc.polkachu.com', rest: 'https://rest.cosmos.directory/osmosis' }
+  const chihuahua_chain = {
+    ...chihuahua,
+    rpc: 'https://chihuahua-rpc.polkachu.com',
+    rest: 'https://rest.cosmos.directory/chihuahua'
+  }
+  const osmosis_chain = {
+    ...osmosis,
+    rpc: 'https://osmosis-rpc.polkachu.com',
+    rest: 'https://rest.cosmos.directory/osmosis'
+  }
 
   return (
     <QueryClientProvider client={queryClient}>
-    <RecoilRoot>
-      <ThemeProvider theme={darkTheme}>
-        <CssBaseline />
-        <ModalProvider>
-          <SnackbarProvider>
+      <RecoilRoot>
+        <ThemeProvider theme={darkTheme}>
+          <CssBaseline />
+          <ModalProvider>
+            <SnackbarProvider>
               <GrazProvider
                 grazOptions={{
                   chains: [osmosis_chain, chihuahua_chain],
                   defaultWallet: WalletType.KEPLR
                 }}
               >
-                <AppProvider>
-                  <UserProvider>{children}</UserProvider>
-                </AppProvider>
+                <AppProvider>{children}</AppProvider>
               </GrazProvider>
-          </SnackbarProvider>
-        </ModalProvider>
-      </ThemeProvider>
-    </RecoilRoot>
+            </SnackbarProvider>
+          </ModalProvider>
+        </ThemeProvider>
+      </RecoilRoot>
     </QueryClientProvider>
   )
 }
