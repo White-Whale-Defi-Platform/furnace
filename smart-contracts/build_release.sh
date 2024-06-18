3b8582e4a9cd26b1c64a127ae64d3a8fffd33989
Kerber0x <kerber0x@protonmail.com>
2024-06-18 15:33:10 +0100

    chore: add build script

---
+++ smart-contracts/build_release.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -e
+
+projectRootPath=$(realpath "$0" | sed 's|\(.*\)/.*|\1|' | cd ../ | pwd)
+
+# if the operative system is running arm64, append -arm64 to workspace-optimizer. Otherwise not
+arch=$(uname -m)
+
+docker_options=(
+	--rm
+	-v "$projectRootPath":/code
+	--mount type=volume,source="$(basename "$projectRootPath")_cache",target=/target
+	--mount type=volume,source=registry_cache,target=/usr/local/cargo/registry
+)
+
+if [[ "$arch" == "aarch64" || "$arch" == "arm64" ]]; then
+	docker_command=("docker" "run" "${docker_options[@]}" "cosmwasm/optimizer-arm64:0.16.0")
+else
+	docker_command=("docker" "run" "${docker_options[@]}" "cosmwasm/optimizer:0.16.0")
+fi
+
+echo "${docker_command[@]}"
+
+"${docker_command[@]}"
